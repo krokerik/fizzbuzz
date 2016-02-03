257616560e31d14b1a3267916395487d079317d7
Erik Andersson <erik_andersson007@hotmail.com>
2016-02-03 12:11:11 +0100

    Update Project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fizzbuzz "0.1.0-SNAPSHOT"
   :description "Fizz Buzz in Clojure, just trying something simple in the language at first."
-  :url "https://github.com/krokerik"
+  :url "https://github.com/krokerik/fizzbuzz"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
